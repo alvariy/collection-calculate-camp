03f3dd55361ad6404101d9b174a7f789618ab412
IYAN OLIVER ALVAREZ (PLAN-IRIS-ISD-OOCLL/MNL) <ALVARIY@OOCL.COM>
2019-10-09 21:13:18 +0800

    Pass reduce

--- practices/reduce/compare_collections.js
+++ practices/reduce/compare_collections.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function compare_collections(collection_a, collection_b) {
-  return collection_a.reduce(a => (collection_a === collection_b) ? true:false);
+  return collection_a.reduce(a => (JSON.stringify(collection_a) === JSON.stringify(collection_b)));
   //在这里写入代码
 }
 
