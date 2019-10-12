4406826a89389214120367b181b25f3f5329560a
Iyan <iyan@example.com>
2019-10-12 22:49:23 +0800

    Pass some map tests

--- practices/map/rank_asc.js
+++ practices/map/rank_asc.js
@@ -1,6 +1,10 @@
 'use strict';
 var rank_asc = function(collection){
-  return [6,5,4,3,2];
+
+  return collection.sort(function(a,b){
+    console.log(b-a);
+    return b-a;
+  } );
 };
 
 module.exports = rank_asc;
