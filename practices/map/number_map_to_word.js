7184524b85534d06882c59b5e995f286ff9482a4
Iyan <iyan@example.com>
2019-10-12 21:42:15 +0800

    Pass some map tests

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,10 @@
 'use strict';
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+  let alphabet = ['a', 'b', 'c', 'd', 'e', 'f','g','h','i',
+'j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+];
+
+  return collection.map(num => alphabet[num - 1]);
 };
 
 module.exports = number_map_to_word;
