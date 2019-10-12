62d9924ebef08fb0960735187a9d0f2488ad009f
Iyan <iyan@example.com>
2019-10-12 23:35:52 +0800

    Pass find_element

--- practices/reduce/find_element_first_subscript.js
+++ practices/reduce/find_element_first_subscript.js
@@ -2,6 +2,10 @@
 
 function calculate_elements_sum(collection, element) {
   //write code here
+
+  let expected = collection.filter(n => n === element);
+
+  return expected[0];
 }
 
 module.exports = calculate_elements_sum;
