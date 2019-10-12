ce229ba526fecd2d954c32e6769559e73b725319
Iyan <iyan@example.com>
2019-10-12 23:55:35 +0800

    pass reduce tests

--- practices/reduce/find_first_even.js
+++ practices/reduce/find_first_even.js
@@ -2,6 +2,24 @@
 
 function find_first_even(collection) {
   //write code here
+
+  var count = 0;
+  return collection.reduce((a,b) =>
+  {
+
+    if(b % 2 === 0 && count === 0)
+    {
+      count++;
+      a=b;
+      return a;
+    }
+    else
+    {
+    return a;
+    }
+    
+  }
+  );
 }
 
 module.exports = find_first_even;
--- practices/reduce/find_last_even.js
+++ practices/reduce/find_last_even.js
@@ -2,6 +2,18 @@
 
 function find_last_even(collection) {
   //write code here
+  return collection.reduce((a,b) =>
+  {
+    if(b % 2 == 0)
+    {
+
+      a=b;
+      return a;
+    }
+    else
+    return a;
+  }
+  );
 }
 
 module.exports = find_last_even;
